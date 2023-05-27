cc649afd0aa6c646a54580334d99a0aeeb835add
RandomGamingDev <ethanshiyi1123@gmail.com>
2023-05-27 10:29:17 -0400

    gave more mipmap & texture control

---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6c804.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cmTC_6c804.dir/CMakeCCompilerABI.c.obj"
+  "cmTC_6c804.exe"
+  "cmTC_6c804.exe.manifest"
+  "cmTC_6c804.pdb"
+  "libcmTC_6c804.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cmTC_6c804.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/CMakeTmp/CMakeFiles/cmTC_6c804.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for cmTC_6c804.
+# This may be replaced when dependencies are built.
